8a0c201213d5a2421a3ecda9db2a81f3de38e6b3
haroldsphinx <adedayoakinpelu@gmail.com>
2022-02-09 18:09:20 +0100

    Uodate cherry pick strategy

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,6 +37,6 @@ git_setup
 git_cmd git remote update
 git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
-git_cmd git cherry-pick "${GITHUB_SHA}"
+git cherry-pick --strategy=recursive -X ours "${GITHUB_SHA}"
 git_cmd git push -u origin "${PR_BRANCH}"
 git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
